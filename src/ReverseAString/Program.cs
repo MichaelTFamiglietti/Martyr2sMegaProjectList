036452338d0efb4c5801014bc00048e2e28c0c1e
Michael Taylor Famiglietti <MichaelTFamiglietti@gmail.com>
2013-08-04 14:11:12 -0400

    Finished Reverse A String

---
+++ src/ReverseAString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String to reverse: ");
+            Console.WriteLine(ReverseString(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string toBeRevd)
+        {
+            string toReturn = "";       //The new reversed string
+
+            //Loops backwards through the string
+            for (int i = toBeRevd.Count() - 1; i >= 0; i--)
+                toReturn += toBeRevd[i];
+
+            return toReturn;
+        }
+
+    }
+}
